b1d9a5dcb7dd6dad649560a830f32dc86364397b
Irene Alvarado <irealva@users.noreply.github.com>
2021-04-06 11:21:59 -0700

    Create postprocess.ts

---
+++ postprocess.ts
@@ -0,0 +1,9 @@
+const filename = Deno.args[0]
+const decoder = new TextDecoder('utf-8')
+const encoder = new TextEncoder()
+const raw = await Deno.readFile(filename)
+const data = JSON.parse(decoder.decode(raw))
+const newFilename = `post_${filename}`
+await Deno.writeFile(newFilename, encoder.encode(JSON.stringify(data.content)))
+console.log('I AM NOT THE FILENAME')
+console.log(newFilename)

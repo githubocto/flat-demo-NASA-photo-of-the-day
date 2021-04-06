ec301fce867d8b6498ac650d0332eec0e9b0c13b
Irene Alvarado <irealva@users.noreply.github.com>
2021-04-06 12:32:11 -0700

    Update postprocess.ts

--- postprocess.ts
+++ postprocess.ts
@@ -1,9 +1,10 @@
-const filename = Deno.args[0]
 const decoder = new TextDecoder('utf-8')
 const encoder = new TextEncoder()
-const raw = await Deno.readFile(filename)
+const raw = await Deno.readFile('data.json')
 const data = JSON.parse(decoder.decode(raw))
-const newFilename = `post_${filename}`
-await Deno.writeFile(newFilename, encoder.encode(JSON.stringify(data)))
-console.log('I AM NOT THE FILENAME')
-console.log(newFilename)
+const imageURL = data.url
+const res = await fetch(imageURL);
+const imageBytes = new Uint8Array(await res.arrayBuffer());
+const newFileName = './nasa-image-of-the-day.jpg'
+await Deno.writeFile(newFileName, imageBytes);
+console.log(newFileName)

16ebe56424f3aa9c20570945a5b175b9c86f662d
Irene Alvarado <irealva@users.noreply.github.com>
2021-04-06 12:47:28 -0700

    Update postprocess.ts

--- postprocess.ts
+++ postprocess.ts
@@ -3,9 +3,12 @@ const decoder = new TextDecoder('utf-8')
 const encoder = new TextEncoder()
 const raw = await Deno.readFile(filename)
 const data = JSON.parse(decoder.decode(raw))
+console.log(data)
 const imageURL = data.url
 const res = await fetch(imageURL);
+console.log(res)
 const imageBytes = new Uint8Array(await res.arrayBuffer());
+console.log(imageBytes)
 const newFileName = './nasa-image-of-the-day.jpg'
 await Deno.writeFile(newFileName, imageBytes);
 console.log(newFileName)

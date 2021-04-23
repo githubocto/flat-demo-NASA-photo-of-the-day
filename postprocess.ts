74307b808d97f29f5e75046bbece160f4bf26785
Irene Alvarado <irealva@users.noreply.github.com>
2021-04-23 14:00:34 -0700

    Update postprocess.ts

--- postprocess.ts
+++ postprocess.ts
@@ -1,17 +1,19 @@
-// You can test this script locally on your computer by runinng `deno run -A postprocess.ts data.json`
+// You can test this script locally on your computer by runinng `deno run -A --unstable postprocess.ts data.json`
+import { readJSON, readImageFromURL, writeImage } from 'https://deno.land/x/flat@0.0.8/mod.ts'
 
-import { readJSON } from 'https://deno.land/x/flat@0.0.2/mod.ts'
-
-// required: get the data filename as the first argument
+// Get the data filename as the first argument
 const filename = Deno.args[0]
-const data = await readJSON(filename) as any // cast as any so we can read any keys from JSON
+const data = await readJSON(filename)
+const imageURL = data.url // fetch the URL key in the json
 
 // Postprocess steps
-const imageURL = data.url // fetch the URL key in the json
-const response = await fetch(imageURL); // fetch an image
-const imageBytes = new Uint8Array(await response.arrayBuffer());
-const newFileName = './nasa-image-of-the-day.jpg' // filename to save the image data
-await Deno.writeFile(newFileName, imageBytes); // create a jpg file with Deno
 
-// IMPORTANT: we have output the new filename as the last log
-console.log(newFileName)
+// Case 1 it is a normal image
+if (!imageURL.startsWith('https://www.youtube.com')) {
+    const image = await readImageFromURL(imageURL) // fetch the image
+    await writeImage(image.bytes, './', 'nasa-image-of-the-day.jpg') // create a jpg file
+} 
+// Case 2 it is a link to youtube
+else { 
+    console.log("Today has no image, but a link to youtube: ", imageURL)
+}

b20578d03a4ea84d73801ea9c096acbf770fe023
Irene Alvarado <irealva@users.noreply.github.com>
2021-04-25 13:37:34 -0700

    Update postprocess.ts

--- postprocess.ts
+++ postprocess.ts
@@ -1,3 +1,5 @@
+// Helper library written for useful postprocessing tasks with Flat Data
+// Has helper functions for manipulating csv, txt, json, excel, zip, and image files
 // You can test this script locally on your computer by runinng `deno run -A --unstable postprocess.ts data.json`
 import { readJSON, readImageFromURL, writeImage } from 'https://deno.land/x/flat@0.0.10/mod.ts'
 
